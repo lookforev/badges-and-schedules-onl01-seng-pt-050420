79ad45c5fdef92c82893620722f8eacf63cf9a48
Evan Bernard <epaulbernard@gmail.com>
2020-05-11 20:21:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,8 @@ end
 #speaker = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 
 def batch_badge_creator()
-  
+  speaker = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
   #array each 
   #puts out "each componet in array into badge_maker"
+end
+batch_badge_creator
