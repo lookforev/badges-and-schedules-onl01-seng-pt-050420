a00a42fe795ddb61759cee3445b3dda0232fe3ec
Evan Bernard <epaulbernard@gmail.com>
2020-05-11 20:30:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,9 @@
 # Write your code here.
-def badge_maker(name)
+def badge_maker()
   puts "Hello, my name is #{name}."
 end
 
 def batch_badge_creator()
   speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
-  speakers.each { |speaker| badge_maker(speaker) }
+  speakers.each { |name| badge_maker() }
 end
